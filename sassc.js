ccb77531637dd722db1c79e04a8665b7b4a411a7
Davide Perozzi <davide@dorfjungs.com>
2017-02-13 18:38:28 +0100

    ADD timeout and callback support for sassc pipe

--- sassc.js
+++ sassc.js
@@ -1,17 +1,28 @@
 const gulp = require('gulp');
 const rename = require('gulp-rename');
 const run = require('gulp-run');
+const exec = require('gulp-exec');
 
 module.exports = (config) => {
-	gulp.task('sassc-compile', () => {
-		for (var i = 0, len = config.files.length; i < len; i++) {
-			var files = config.files[i];
-			var stream = gulp.src(files.entry)
-				.pipe(run('sassc -t compressed -I ' + config.watch + (config.paths ? (':' + config.paths.join(':')) : ''), {verbosity: 1}))
-				.on('error', (err) => { stream.end(); })
-			    .pipe(rename((path) => { path.extname = ".css"; }))
-			    .pipe(gulp.dest(files.output));
-		}
+	gulp.task('sassc-compile', (callback) => {
+		setTimeout(() => {
+			let promises = [];
+
+			for (let i = 0, len = config.files.length; i < len; i++) {
+				promises.push(new Promise((resolve, reject) => {
+					let files = config.files[i];
+					let paths = config.watch + (config.paths ? (':' + config.paths.join(':')) : '');
+					let stream = gulp.src(files.entry)
+						.pipe(run('sassc -t compressed -I ' + paths, {verbosity: 1}))
+						.on('error', (err) => {stream.end();reject(err);})
+						.pipe(rename((path) => { path.extname = ".css"; }))
+						.pipe(gulp.dest(files.output))
+						.on('finish', () => {setTimeout(resolve, 0);});
+				}));
+			}
+
+			Promise.all(promises).then(() => callback());
+		}, !isNaN(config.timeout) ? config.timeout : 50);
 	});
 
 	gulp.task('sassc-watch', () => {
